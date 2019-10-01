9fc71454257ee948909769ec75d99a0812047cfc
Tyler <tylerhines1992@gmail.com>
2019-09-30 21:20:46 -0400

    added view for soloqueue specifically

--- src/components/SummonerSearch.js
+++ src/components/SummonerSearch.js
@@ -49,26 +49,33 @@ class SummonerSearch extends React.Component {
 				<div className="ui label">
 					Level: <div className="detail">{this.state.summonerLevel}</div>
 				</div>
-				<div className="ui label">
-					Rank:{" "}
-					<div className="detail">
-						{this.state.soloQueueRank.soloQueue.rank}
-					</div>
-				</div>
-				<div className="ui label">
-					LP:{" "}
-					<div className="detail">{this.state.soloQueueRank.soloQueue.LP}</div>
-				</div>
-				<div className="ui label">
-					Wins:{" "}
-					<div className="detail">
-						{this.state.soloQueueRank.soloQueue.wins}
-					</div>
-				</div>
-				<div className="ui label">
-					Losses:{" "}
-					<div className="detail">
-						{this.state.soloQueueRank.soloQueue.losses}
+				<div className="ui segment">
+					<div className="ui label">
+						Solo Queue:
+						<div className="ui label">
+							Rank:{" "}
+							<div className="detail">
+								{this.state.soloQueueRank.soloQueue.rank}
+							</div>
+						</div>
+						<div className="ui label">
+							LP:{" "}
+							<div className="detail">
+								{this.state.soloQueueRank.soloQueue.LP}
+							</div>
+						</div>
+						<div className="ui label">
+							Wins:{" "}
+							<div className="detail">
+								{this.state.soloQueueRank.soloQueue.wins}
+							</div>
+						</div>
+						<div className="ui label">
+							Losses:{" "}
+							<div className="detail">
+								{this.state.soloQueueRank.soloQueue.losses}
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
