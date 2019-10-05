184649d40fb6ed553f768007deda2574119da1e5
Tyler <tylerhines1992@gmail.com>
2019-10-05 18:07:06 -0400

    updated api key

--- src/api/RiotGames.js
+++ src/api/RiotGames.js
@@ -5,7 +5,7 @@ export default axios.create({
 	headers: {
 		Origin: "https://developer.riotgames.com",
 		"Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
-		"X-Riot-Token": "RGAPI-0d02d9a4-4f3c-4641-b7f1-f36c72031cb4",
+		"X-Riot-Token": "RGAPI-656e2cc6-361c-49bb-8a11-b4858c856d50",
 		"Accept-Language": "en-US,en;q=0.9",
 		"User-Agent":
 			"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36"
